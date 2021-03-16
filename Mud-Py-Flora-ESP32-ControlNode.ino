3762536e01508e63bab670c8c6b895dab164cc9e
Joseph Eoff <joseph.eoff@molear.de>
2021-03-16 22:04:33 +0100

    Implemented WLAN and MQTT connection. Implemented node battery message.

--- Mud-Py-Flora-ESP32-ControlNode.ino
+++ Mud-Py-Flora-ESP32-ControlNode.ino
@@ -1,35 +1,105 @@
+#include <esp_task_wdt.h>
 #include "BLEDevice.h"
 #include <WiFi.h>
 #include <PubSubClient.h>
 
 #include "config.h"
 
+WiFiClient espWLAN;
+PubSubClient MQTTclient(espWLAN);
+
+const int BatteryMonitor_ADC_pin = 36;
+
 void setup() {
+  setupWatchdog();
   float nodeBatteryVoltage = getNodeBatteryVoltage();
   connectToServer();
-  subscribeToSensorIDMessage();
+  //subscribeToSensorIDMessage();
   sendActiveNotice(nodeBatteryVoltage);
-  string() sensorIDs = readSensorIDListFromServer();
+  //string() sensorIDs = readSensorIDListFromServer();
   disconnectFromServer();
-  readSensorDataOverBluetooth();
-  connectToServer();
-  subscribeToSleepMessage();
-  sendSensorUpdates();
-  sendDone();
-  int sleepTimeSeconds = readSleepPeriodFromMessage();
-  disconnectFromServer();
-  deepSleep(sleepTimeSeconds);  
+  //readSensorDataOverBluetooth();
+  //connectToServer();
+  //subscribeToSleepMessage();
+  //sendSensorUpdates();
+  //sendDone();
+  //int sleepTimeSeconds = readSleepPeriodFromMessage();
+  //disconnectFromServer();
+  //takeANap(sleepTimeSeconds);  
+  takeANap(30);
 
+}
+void setupWatchdog(){
+   esp_task_wdt_init(WATCHDOG_TIMEOUT_SECONDS, false);
+   esp_task_wdt_add(NULL);
+}
+
+void esp_task_wdt_isr_user_handler(void){
+  takeANap(WATCHDOG_TIMEOUT_SECONDS);
+}
+
+void takeANap(int sleepTimeSeconds){
+  esp_sleep_enable_timer_wakeup(sleepTimeSeconds * 1000000);
+  esp_deep_sleep_start();
+}
+
+float getNodeBatteryVoltage(){
+  int ADC_VALUE = analogRead(BatteryMonitor_ADC_pin);
+  float voltage_value = (((float)ADC_VALUE) * 3.3 ) / (4095.0) * 2;
+  return voltage_value;
 }
 
 void connectToServer(){
   connectWLAN();
+  esp_task_wdt_reset();
   connectMQTT();
+  esp_task_wdt_reset();
 }
 
+void connectWLAN(){
+  WiFi.begin(WLAN_SSID, WLAN_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
+void connectMQTT(){
+  MQTTclient.setServer(MQTT_HOST, MQTT_PORT);
+  while (!MQTTclient.connected()) {
+    if (!MQTTclient.connect(MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD)) {
+      delay(MQTT_RETRY_WAIT);
+    }
+  }
+}
+  
 void disconnectFromServer(){
   disconnectMQTT();
-  disconectWLAN();
+  esp_task_wdt_reset();
+  disconnectWLAN();
+  esp_task_wdt_reset();
+}
+
+void disconnectWLAN(){
+    WiFi.disconnect(true);
+}
+
+void disconnectMQTT(){
+    MQTTclient.disconnect();
+}
+
+void sendActiveNotice(float batteryVoltage){
+  publishMQTTMessage(MQTT_NODE_TOPIC,WiFi.macAddress(), MQTT_BATTERY_TOPIC, String(batteryVoltage));
+}
+
+void publishMQTTMessage(String baseTopic, String ID, String topic, String content){
+  char buffer[64];
+  
+  esp_task_wdt_reset();
+
+  snprintf(buffer, 64, content.c_str());
+  MQTTclient.publish((baseTopic+ "/" + ID + "/" +topic).c_str(), buffer); 
+  
+  esp_task_wdt_reset();
 }
 
 void loop() {
--- config-example.h
+++ config-example.h
@@ -1,5 +1,8 @@
 //Example configuration for the Mud-Py flora ControlNode
 
+const int     WATCHDOG_TIMEOUT_SECONDS = 60;
+const int     RETRYPERIOD_SECONDS = 300;
+
 const char*   WLAN_SSID       = "SomeSSID";
 const char*   WLAN_PASSWORD   = "SomePassword";
 
@@ -8,5 +11,7 @@ const int     MQTT_PORT       = 1883;
 const char*   MQTT_CLIENTID   = "mud-py-node1";
 const char*   MQTT_USERNAME   = "username";
 const char*   MQTT_PASSWORD   = "password";
-const String  MQTT_BASE_TOPIC = "mud-py-flora"; 
+const String  MQTT_NODE_TOPIC = "mud-py-node"; 
+const String  MQTT_BATTERY_TOPIC = "battery";
+const String  MQTT_SENSOR_TOPIC = "mud-py-flora"; 
 const int     MQTT_RETRY_WAIT = 5000;
